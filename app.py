9eac44f35c14443bfabbcd606eb7e901244aacad
Anjana <anjanapknr@gmail.com>
2021-11-14 17:56:21 -0500

    upload

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/login")
+def login():
     return render_template("login.html")
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
